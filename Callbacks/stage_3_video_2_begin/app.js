022d0f35a54b4c2419f457c9333a5a2763f4e65d
Stephen Ditta <stephenditta@Stephens-MacBook-Pro.local>
2018-03-27 19:35:02 -0400

    callback arguments practice

--- Callbacks/stage_3_video_2_begin/app.js
+++ Callbacks/stage_3_video_2_begin/app.js
@@ -1,10 +1,15 @@
 const nameInput = document.getElementById('name');
 const messageTextArea = document.getElementById('message');
 
-nameInput.addEventListener('focus', event => {
+const focusHandler = function(){
   event.target.className = 'highlight';
-});
-
-nameInput.addEventListener('blur', event => {
+};
+const blurHandler = function(){
   event.target.className = '';
-});
+};
+
+nameInput.addEventListener('focus', focusHandler);
+nameInput.addEventListener('blur', blurHandler);
+
+messageTextArea.addEventListener('focus', focusHandler);
+messageTextArea.addEventListener('blur', blurHandler);
